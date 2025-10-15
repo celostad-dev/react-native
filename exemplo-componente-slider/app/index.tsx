47aa044b2f92c70bcbc9fad10b263015f055a1d7
mtadim <mtadim@techsocial.com.br>
2025-10-15 19:04:30 -0300

    29.Slider

---
+++ exemplo-componente-slider/app/index.tsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet} from 'react-native';
+
+import Slider from '@react-native-community/slider';
+
+export default class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = { 
+      valor: 50
+    };
+  };
+
+ render(){  
+
+  return (
+    <View style={styles.container}>
+
+      <Slider 
+        minimunValue={0}
+        maximumValue={100}
+        onValueChange={ (valorSelecionado) => this.setState({valor: valorSelecionado})}
+        value={this.state.valor}
+        minimumTrackTintColor='#00ff00'
+        maximumTrackTintColor='#ff0000'
+      />
+      <Text style={{ textAlign: 'center', fontSize: 30 }}>
+        { this.state.valor.toFixed(1)}
+      </Text>
+
+      </View>
+   );
+ }
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    marginTop: 50,
+  }
+});
